8a919eb173167e8deee4deff23fd51303096b6d9
Dedipyaman Das <30053071+2DSharp@users.noreply.github.com>
2020-05-13 22:16:35 +0530

    Update interface+impl for LocationRepository

--- src/main/java/me/twodee/friendlyneighbor/repository/LocationRepository.java
+++ src/main/java/me/twodee/friendlyneighbor/repository/LocationRepository.java
@@ -1,15 +1,23 @@
 package me.twodee.friendlyneighbor.repository;
 
-import me.twodee.friendlyneighbor.entity.Location;
+import me.twodee.friendlyneighbor.entity.UserLocation;
+
+import java.util.List;
 
 public interface LocationRepository
 {
     /**
-     * Saves a user's location data
-     * @param location
-     * @return
+     * Saves a user's userLocation data
+     * @param userLocation User data with location/radius information in KM
+     * @return Returns the persisted user location
      */
-    Location save(Location location);
+    UserLocation save(UserLocation userLocation);
 
+    /**
+     * Look up users who are near a given user who is already registered
+     * @param userId The user looking up nearby users
+     * @return A list of users with their distance
+     */
+    List<UserLocation> getUsersNearBy(String userId);
 
 }
--- src/main/java/me/twodee/friendlyneighbor/repository/MongoLocationRepository.java
+++ src/main/java/me/twodee/friendlyneighbor/repository/MongoLocationRepository.java
@@ -1,10 +1,20 @@
 package me.twodee.friendlyneighbor.repository;
 
-import me.twodee.friendlyneighbor.entity.Location;
+import me.twodee.friendlyneighbor.entity.UserLocation;
+import org.springframework.data.geo.*;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.AggregationResults;
+import org.springframework.data.mongodb.core.aggregation.GeoNearOperation;
+import org.springframework.data.mongodb.core.aggregation.MatchOperation;
 import org.springframework.data.mongodb.core.index.GeospatialIndex;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.NearQuery;
 
 import javax.inject.Inject;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class MongoLocationRepository implements LocationRepository
 {
@@ -19,12 +29,60 @@ public class MongoLocationRepository implements LocationRepository
 
     private void initTemplate()
     {
-        template.indexOps(Location.class).ensureIndex(new GeospatialIndex("position"));
+        template.indexOps(UserLocation.class).ensureIndex(new GeospatialIndex("position"));
     }
 
     @Override
-    public Location save(Location location)
+    public UserLocation save(UserLocation userLocation)
     {
-        return template.save(location);
+        return template.save(userLocation);
     }
+
+    @Override
+    public List<UserLocation> getUsersNearBy(String userId)
+    {
+        UserLocation userLocation = template.findById(userId, UserLocation.class);
+
+        if (userLocation != null) {
+            return getUsersInGivenLocation(userLocation.getPosition(), userLocation.getRadius(), userId);
+        }
+        return Collections.emptyList();
+    }
+
+    private List<UserLocation> getUsersInGivenLocation(UserLocation.Position position, double radius, String exclude)
+    {
+        GeoResults<UserLocation> geoResults = template.query(UserLocation.class)
+            .as(UserLocation.class)
+            .near(createNearQuery(position, radius))
+            .all();
+
+        return geoResults.getContent().stream()
+                .filter(result -> filterIneligibleResults(result, exclude))
+                .map(this::setDistanceInGeoResult)
+                .collect(Collectors.toList());
+    }
+
+    private boolean filterIneligibleResults(GeoResult<UserLocation> result, String exclude)
+    {
+        return (result.getDistance().in(Metrics.KILOMETERS).getValue() <= result.getContent().getRadius())
+                && (!result.getContent().getId().equals(exclude));
+    }
+
+    private UserLocation setDistanceInGeoResult(GeoResult<UserLocation> locationGeoResult)
+    {
+        UserLocation location = locationGeoResult.getContent();
+        location.setDis(locationGeoResult.getDistance().in(Metrics.KILOMETERS).getValue());
+        return location;
+    }
+
+
+    private NearQuery createNearQuery(UserLocation.Position position, double radius)
+    {
+        Point location = new Point(position.getLongitude(), position.getLatitude());
+        Distance distance = new Distance(radius, Metrics.KILOMETERS);
+
+        return NearQuery.near(location).maxDistance(distance);
+    }
+
+
 }
