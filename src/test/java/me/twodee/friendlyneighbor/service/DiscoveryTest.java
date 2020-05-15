6fbc60d7804a2c061f1fc9403ca5f1f1445d06fd
Dedipyaman Das <30053071+2DSharp@users.noreply.github.com>
2020-05-15 21:32:39 +0530

    Add tests for save/delete

--- src/test/java/me/twodee/friendlyneighbor/service/DiscoveryTest.java
+++ src/test/java/me/twodee/friendlyneighbor/service/DiscoveryTest.java
@@ -1,7 +1,8 @@
 package me.twodee.friendlyneighbor.service;
 
 import me.twodee.friendlyneighbor.FnCoreGenerated;
-import me.twodee.friendlyneighbor.dto.UserLocationsDTO;
+import me.twodee.friendlyneighbor.dto.ResultObject;
+import me.twodee.friendlyneighbor.dto.UserLocationsResult;
 import me.twodee.friendlyneighbor.entity.UserLocation;
 import me.twodee.friendlyneighbor.exception.InvalidUser;
 import me.twodee.friendlyneighbor.repository.LocationRepository;
@@ -18,6 +19,8 @@ import java.util.List;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
 
 class DiscoveryTest
@@ -31,6 +34,7 @@ class DiscoveryTest
         MockitoAnnotations.initMocks(this);
     }
 
+    // TODO: Remove this
     FnCoreGenerated.SearchAreaRequest getRequest()
     {
         FnCoreGenerated.Location location = FnCoreGenerated.Location.newBuilder()
@@ -53,7 +57,7 @@ class DiscoveryTest
 
         UserLocation loc = new UserLocation("abc123", new UserLocation.Position(22.507449, 88.34), 2100);
 
-        UserLocationsDTO result = discovery.lookupNearbyUsersByLocation(loc);
+        UserLocationsResult result = discovery.lookupNearbyUsersByLocation(loc);
 
         assertTrue(result.getNotification().hasErrors());
         assertTrue(result.getNotification().getErrors().containsKey("userId"));
@@ -78,7 +82,7 @@ class DiscoveryTest
 
         UserLocation searchLoc = new UserLocation("abc123", new UserLocation.Position(22.507449, 88.34), 2100);
 
-        UserLocationsDTO nearbyUsers = discovery.lookupNearbyUsersByLocation(searchLoc);
+        UserLocationsResult nearbyUsers = discovery.lookupNearbyUsersByLocation(searchLoc);
 
         assertFalse(nearbyUsers.getNotification().hasErrors());
         assertThat(nearbyUsers.getUserLocations().size(), equalTo(2));
@@ -95,7 +99,7 @@ class DiscoveryTest
         Discovery discovery = new Discovery(repository);
         String requestingUid = "abc123";
 
-        UserLocationsDTO result = discovery.lookupNearbyUsersByUserId(requestingUid);
+        UserLocationsResult result = discovery.lookupNearbyUsersByUserId(requestingUid);
 
         assertTrue(result.getNotification().hasErrors());
         assertFalse(result.getNotification().getErrors().isEmpty());
@@ -120,7 +124,7 @@ class DiscoveryTest
         Discovery discovery = new Discovery(repository);
         String requestingUid = "abc123";
 
-        UserLocationsDTO nearbyUsers = discovery.lookupNearbyUsersByUserId(requestingUid);
+        UserLocationsResult nearbyUsers = discovery.lookupNearbyUsersByUserId(requestingUid);
 
         assertFalse(nearbyUsers.getNotification().hasErrors());
         assertThat(nearbyUsers.getUserLocations().size(), equalTo(2));
@@ -128,5 +132,52 @@ class DiscoveryTest
                 .containsOnly("abc123", "hello");
         Assertions.assertThat(nearbyUsers.getUserLocations()).extracting("distance")
                 .containsOnly(10, 2.0);
+    }
+
+    @Test
+    void testUserSave()
+    {
+        Discovery discovery = new Discovery(repository);
+        UserLocation loc = new UserLocation("abc", new UserLocation.Position(10, 10), 10);
+
+        ResultObject res = discovery.saveUserLocation(loc);
+
+        assertFalse(res.getNotification().hasErrors());
+    }
+
+    @Test
+    void testUserSaveFailure()
+    {
+        UserLocation loc = new UserLocation("abc", new UserLocation.Position(10, 10), 10);
+
+        when(repository.save(any())).thenThrow(RuntimeException.class);
+        Discovery discovery = new Discovery(repository);
+
+        ResultObject res = discovery.saveUserLocation(loc);
+
+        assertTrue(res.getNotification().hasErrors());
+        assertThat(res.getNotification().getErrors().get("internal"), equalTo(ResultObject.SOMETHING_WENT_WRONG));
+    }
+
+    @Test
+    void testUserDelete()
+    {
+        Discovery discovery = new Discovery(repository);
+
+        ResultObject res = discovery.deleteUserLocation("abc");
+
+        assertFalse(res.getNotification().hasErrors());
+    }
+
+    @Test
+    void testUserDeleteFailure()
+    {
+        doThrow(RuntimeException.class).when(repository).deleteById(any());
+        Discovery discovery = new Discovery(repository);
+
+        ResultObject res = discovery.deleteUserLocation("abc");
+
+        assertTrue(res.getNotification().hasErrors());
+        assertThat(res.getNotification().getErrors().get("internal"), equalTo(ResultObject.SOMETHING_WENT_WRONG));
     }
 }
