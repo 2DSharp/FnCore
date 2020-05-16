3ec7f79d26c09206d5c2895f62fef41b47a3f095
Dedipyaman Das <30053071+2DSharp@users.noreply.github.com>
2020-05-16 17:05:30 +0530

    Remove outdated methods; Migration complete

--- src/main/java/me/twodee/friendlyneighbor/service/Discovery.java
+++ src/main/java/me/twodee/friendlyneighbor/service/Discovery.java
@@ -1,7 +1,6 @@
 package me.twodee.friendlyneighbor.service;
 
 import lombok.extern.java.Log;
-import me.twodee.friendlyneighbor.FnCoreGenerated;
 import me.twodee.friendlyneighbor.dto.Notification;
 import me.twodee.friendlyneighbor.dto.ResultObject;
 import me.twodee.friendlyneighbor.dto.UserLocationsResult;
@@ -11,10 +10,6 @@ import me.twodee.friendlyneighbor.repository.LocationRepository;
 
 import javax.inject.Inject;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @Log
 public class Discovery
@@ -81,45 +76,4 @@ public class Discovery
         return new UserLocationsResult(note);
     }
 
-    private FnCoreGenerated.NearbyUsersResult buildFailedResult(Throwable e)
-    {
-        return FnCoreGenerated.NearbyUsersResult
-                .newBuilder()
-                .setMetaResult(FnCoreGenerated.RequestResult.newBuilder()
-                                       .setSuccess(false)
-                                       .putAllErrors(getErrors(e))
-                                       .build()).build();
-    }
-
-    Map<String, String> getErrors(Throwable e)
-    {
-        Map<String, String> errors = new HashMap<>();
-        if (e instanceof InvalidUser) {
-            errors.put("userId", "The supplied User ID doesn't exist");
-        }
-        return errors;
-    }
-
-    private FnCoreGenerated.UserNearby createDtoUser(UserLocation userLocation)
-    {
-        return FnCoreGenerated.UserNearby.newBuilder()
-                .setDistance(userLocation.getDistance().doubleValue())
-                .setUserId(userLocation.getId())
-                .build();
-    }
-
-    private FnCoreGenerated.NearbyUsersResult buildResult(List<FnCoreGenerated.UserNearby> results)
-    {
-        return FnCoreGenerated.NearbyUsersResult.newBuilder()
-                .setMetaResult(FnCoreGenerated.RequestResult.newBuilder().setSuccess(true).build())
-                .addAllUser(results)
-                .build();
-    }
-
-    private List<FnCoreGenerated.UserNearby> buildUserList(List<UserLocation> users)
-    {
-        return users.stream()
-                .map(this::createDtoUser)
-                .collect(Collectors.toList());
-    }
 }
