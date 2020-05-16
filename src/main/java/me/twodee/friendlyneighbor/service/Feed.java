1c7c3d9b20a1f9ee3ef183e24cdecba2868a8446
Dedipyaman Das <30053071+2DSharp@users.noreply.github.com>
2020-05-16 17:41:44 +0530

    Build feed service from contract

---
+++ src/main/java/me/twodee/friendlyneighbor/service/Feed.java
@@ -0,0 +1,51 @@
+package me.twodee.friendlyneighbor.service;
+
+import lombok.extern.java.Log;
+import me.twodee.friendlyneighbor.dto.PostResults;
+import me.twodee.friendlyneighbor.dto.ResultObject;
+import me.twodee.friendlyneighbor.dto.SuccessResult;
+import me.twodee.friendlyneighbor.dto.UserLocationsResult;
+import me.twodee.friendlyneighbor.entity.Post;
+import me.twodee.friendlyneighbor.entity.UserLocation;
+import me.twodee.friendlyneighbor.repository.PostRepository;
+
+import javax.inject.Inject;
+
+@Log
+public class Feed
+{
+    private final Discovery discovery;
+    private final PostRepository repository;
+
+    @Inject
+    public Feed(Discovery discovery, PostRepository repository)
+    {
+        this.discovery = discovery;
+        this.repository = repository;
+    }
+
+    public ResultObject pushRequestToNearbyUsers(String postId, UserLocation currentUserLocation)
+    {
+        repository.save(new Post(postId, currentUserLocation));
+        UserLocationsResult usersNearby = discovery.lookupNearbyUsersByLocation(currentUserLocation);
+        repository.forwardToUsers(usersNearby.getUserLocations());
+
+        return new SuccessResult();
+    }
+
+    public ResultObject pushRequestToNearbyUsers(String postId, String userId)
+    {
+        UserLocation currentUserLocation = discovery.getUserLocation(userId);
+        repository.save(new Post(postId, currentUserLocation));
+        UserLocationsResult usersNearby = discovery.lookupNearbyUsersByLocation(currentUserLocation);
+        repository.forwardToUsers(usersNearby.getUserLocations());
+
+        return new SuccessResult();
+    }
+
+    public PostResults fetchRequestsForUser(String userId)
+    {
+        return new PostResults(repository.findAllForUser(userId));
+
+    }
+}
