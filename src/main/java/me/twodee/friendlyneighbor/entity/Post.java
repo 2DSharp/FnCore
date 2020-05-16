9e571d0bcf11c9924c376b72ea239ddaa307ee07
Dedipyaman Das <30053071+2DSharp@users.noreply.github.com>
2020-05-16 19:44:54 +0530

    Store time for posts

--- src/main/java/me/twodee/friendlyneighbor/entity/Post.java
+++ src/main/java/me/twodee/friendlyneighbor/entity/Post.java
@@ -1,6 +1,10 @@
 package me.twodee.friendlyneighbor.entity;
 
 import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -9,6 +13,9 @@ import lombok.*;
 @AllArgsConstructor
 public class Post
 {
+    @Id
     private String id;
+    @Transient
     private UserLocation location;
+    private LocalDateTime time;
 }
