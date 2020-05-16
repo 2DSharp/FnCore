c736e1abf0b83f95bd61092bbbf9f782d4c02c93
Dedipyaman Das <30053071+2DSharp@users.noreply.github.com>
2020-05-16 17:39:57 +0530

    Add a marker for SuccessResults

---
+++ src/main/java/me/twodee/friendlyneighbor/dto/SuccessResult.java
@@ -0,0 +1,12 @@
+package me.twodee.friendlyneighbor.dto;
+
+/**
+ * Marker class to send a ResultObject disguised as SuccessResult
+ */
+public class SuccessResult extends ResultObject
+{
+    public SuccessResult()
+    {
+        super();
+    }
+}
