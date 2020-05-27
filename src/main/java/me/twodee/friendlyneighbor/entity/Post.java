fe9759ec9f2e41d60aed421c424b27499c0d7499
Dedipyaman Das <30053071+2DSharp@users.noreply.github.com>
2020-05-27 19:37:41 +0530

    Add fields for additional full-text-searches

--- src/main/java/me/twodee/friendlyneighbor/entity/Post.java
+++ src/main/java/me/twodee/friendlyneighbor/entity/Post.java
@@ -1,7 +1,14 @@
 package me.twodee.friendlyneighbor.entity;
 
-import lombok.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.mongodb.core.index.TextIndexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.TextScore;
 
 import java.time.LocalDateTime;
 
@@ -9,11 +16,36 @@ import java.time.LocalDateTime;
 @Setter
 @NoArgsConstructor
 @ToString
-@AllArgsConstructor
-public class Post
-{
+@Document
+public class Post {
     @Id
     private String id;
     private UserLocation location;
     private LocalDateTime time;
+    private PostType type;
+    @TextIndexed(weight = 2)
+    private String title;
+
+    @Transient
+    @TextScore
+    Float score;
+
+    public enum PostType {
+        REQUEST,
+        OFFERING
+    }
+
+    public Post(String id, UserLocation location, LocalDateTime time) {
+        this.id = id;
+        this.location = location;
+        this.time = time;
+    }
+
+    public Post(String id, UserLocation location, LocalDateTime time, PostType type, String title) {
+        this.id = id;
+        this.location = location;
+        this.time = time;
+        this.type = type;
+        this.title = title;
+    }
 }
