4cf697dc657434445c4b2bad29155f258d719c77
Dedipyaman Das <30053071+2DSharp@users.noreply.github.com>
2020-05-27 21:26:06 +0530

    Update public API to add post data

--- src/main/java/me/twodee/friendlyneighbor/FnCoreHandler.java
+++ src/main/java/me/twodee/friendlyneighbor/FnCoreHandler.java
@@ -1,10 +1,7 @@
 package me.twodee.friendlyneighbor;
 
 import io.grpc.stub.StreamObserver;
-import me.twodee.friendlyneighbor.dto.PostResults;
-import me.twodee.friendlyneighbor.dto.ResultObject;
-import me.twodee.friendlyneighbor.dto.UserLocationResult;
-import me.twodee.friendlyneighbor.dto.UserLocationsResult;
+import me.twodee.friendlyneighbor.dto.*;
 import me.twodee.friendlyneighbor.entity.Post;
 import me.twodee.friendlyneighbor.entity.UserLocation;
 import me.twodee.friendlyneighbor.service.Discovery;
@@ -19,7 +16,6 @@ public class FnCoreHandler extends FnCoreGrpc.FnCoreImplBase
 {
     private final Discovery discovery;
     private final Feed feed;
-
     @Inject
     FnCoreHandler(Discovery discovery, Feed feed)
     {
@@ -83,26 +79,37 @@ public class FnCoreHandler extends FnCoreGrpc.FnCoreImplBase
     }
 
     @Override
-    public void findUsersInCircleById(FnCoreGenerated.UserIdentifier request, StreamObserver<FnCoreGenerated.NearbyUsersResult> responseObserver)
-    {
+    public void findUsersInCircleById(FnCoreGenerated.UserIdentifier request, StreamObserver<FnCoreGenerated.NearbyUsersResult> responseObserver) {
         UserLocationsResult result = discovery.lookupNearbyUsersByUserId(request.getUserId());
 
         responseObserver.onNext(buildLocationsResult(result));
         responseObserver.onCompleted();
     }
 
+    private Post.PostType generateDomainType(FnCoreGenerated.PostData.Type type) {
+        switch (type) {
+            case REQUEST:
+                return Post.PostType.REQUEST;
+            case OFFERING:
+                return Post.PostType.OFFERING;
+        }
+        return Post.PostType.REQUEST;
+    }
+
     @Override
-    public void forwardRequestNearbyDefaultLocation(FnCoreGenerated.PostData request, StreamObserver<FnCoreGenerated.Result> responseObserver)
-    {
-        ResultObject result = feed.pushRequestToNearbyUsers(request.getPostId(), request.getUserId());
+    public void forwardRequestNearbyDefaultLocation(FnCoreGenerated.PostData request, StreamObserver<FnCoreGenerated.Result> responseObserver) {
+        ResultObject result = feed.fanoutToNearbyUsers(new PostData(request.getPostId(), request.getTitle(),
+                                                                    generateDomainType(request.getType())),
+                                                       request.getUserId());
         responseObserver.onNext(buildResult(result));
         responseObserver.onCompleted();
     }
 
     @Override
-    public void forwardRequestNearbyCustomLocation(FnCoreGenerated.PostData request, StreamObserver<FnCoreGenerated.Result> responseObserver)
-    {
-        ResultObject result = feed.pushRequestToNearbyUsers(request.getPostId(), buildSearchLocation(request));
+    public void forwardRequestNearbyCustomLocation(FnCoreGenerated.PostData request, StreamObserver<FnCoreGenerated.Result> responseObserver) {
+        ResultObject result = feed.fanoutToNearbyUsers(new PostData(request.getPostId(), request.getTitle(),
+                                                                    generateDomainType(request.getType())),
+                                                       buildSearchLocation(request));
         responseObserver.onNext(buildResult(result));
         responseObserver.onCompleted();
     }
