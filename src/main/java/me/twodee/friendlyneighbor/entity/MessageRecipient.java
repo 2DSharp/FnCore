6539df8f80b4b5d7e955d344ee256924deb657b2
Dedipyaman Das <30053071+2DSharp@users.noreply.github.com>
2020-05-27 21:54:51 +0530

    Add domain classes to hold notification data

---
+++ src/main/java/me/twodee/friendlyneighbor/entity/MessageRecipient.java
@@ -0,0 +1,20 @@
+package me.twodee.friendlyneighbor.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class MessageRecipient {
+
+    @Id
+    String userId;
+    String token;
+}
