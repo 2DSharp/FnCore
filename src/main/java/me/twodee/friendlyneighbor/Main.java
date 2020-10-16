c981a535fb0a3ab76c6bbd4ae625e21211a2c260
2dsharp <30053071+2DSharp@users.noreply.github.com>
2020-10-16 18:26:48 +0530

    Catch all throwables

--- src/main/java/me/twodee/friendlyneighbor/Main.java
+++ src/main/java/me/twodee/friendlyneighbor/Main.java
@@ -16,34 +16,37 @@ import java.util.Properties;
 @Slf4j
 public class Main
 {
-    public static void main(String[] args) throws IOException, InterruptedException
+    public static void main(String[] args)
     {
-        Properties properties = new Properties();
-        if (args.length > 0) {
-            // The user's specified file takes top priority
-            Path path = Paths.get(args[0]);
-            if (Files.exists(path)) {
-                System.out.println(
-                        "Found configuration file! Reading from " + path.getFileName().toAbsolutePath().toString());
-                log.info("Found configuration file! Reading from " + path.getFileName().toAbsolutePath().toString());
-                properties.load(Files.newInputStream(path));
+        try {
+            Properties properties = new Properties();
+            if (args.length > 0) {
+                // The user's specified file takes top priority
+                Path path = Paths.get(args[0]);
+                if (Files.exists(path)) {
+                    System.out.println(
+                            "Found configuration file! Reading from " + path.getFileName().toAbsolutePath().toString());
+                    log.info("Found configuration file! Reading from " + path.getFileName().toAbsolutePath().toString());
+                    properties.load(Files.newInputStream(path));
+                }
+            } else {
+                Path path = Paths.get("fnconfig.properties");
+                // Does a fnconfig already exist? Load it
+                if (Files.exists(path)) {
+                    System.out.println("Found configuration file! Reading from " + path.getFileName().toString());
+                    log.info("Found configuration file! Reading from " + path.getFileName().toAbsolutePath().toString());
+                    properties.load(Files.newInputStream(path));
+                }
             }
+            // If user didn't specify a file and there's no fnconfig, continue with empty props and read defaults
+            FnCoreConfig config = FnCoreConfig.createFromProperties(properties);
+            assert config != null;
+            Injector injector = Guice.createInjector(new LocationModule(config), new NotifierModule(config));
+            FnCoreHandler service = injector.getInstance(FnCoreHandler.class);
+            Server server = new Server(config.getFnCorePort(), service);
+            server.start();
+        } catch (Throwable e) {
+            log.error("Something broke while starting: " + e.getMessage(),  e);
         }
-        else {
-            Path path = Paths.get("fnconfig.properties");
-            // Does a fnconfig already exist? Load it
-            if (Files.exists(path)) {
-                System.out.println("Found configuration file! Reading from " + path.getFileName().toString());
-                log.info("Found configuration file! Reading from " + path.getFileName().toAbsolutePath().toString());
-                properties.load(Files.newInputStream(path));
-            }
-        }
-        // If user didn't specify a file and there's no fnconfig, continue with empty props and read defaults
-        FnCoreConfig config = FnCoreConfig.createFromProperties(properties);
-        assert config != null;
-        Injector injector = Guice.createInjector(new LocationModule(config), new NotifierModule(config));
-        FnCoreHandler service = injector.getInstance(FnCoreHandler.class);
-        Server server = new Server(config.getFnCorePort(), service);
-        server.start();
     }
 }
